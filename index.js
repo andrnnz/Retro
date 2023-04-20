f58f6926d8358db729061c4237070bca15ce7c12
Andrea Núñez <a01640839@itesm.mx>
2023-04-20 18:58:01 -0500

    jquery-bootstrap

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 
 function info(){
-    document.getElementById("btn-info").style.display="none";
+    /*document.getElementById("btn-info").style.display="none";
     document.getElementById("btn-regresar").style.display="inline";
     document.getElementById("stats").style.display="inline";
-    document.getElementById("info-schumacher").style.display="none";
+    document.getElementById("info-schumacher").style.display="none";*/
     document.getElementById("schumacher").src="sf.jpg";
+    $("#btn-info").css("display", "none");
+    $("#btn-regresar").css("display", "inline");
+    $("#stats").css("display", "inline");
+    $("#info-schumacher").css("display", "none");
+    $("#sch2").css("display", "none");
+    $("#container-preStats").css("display", "none");
+
 }
 
 function regresar(){
     document.getElementById("btn-info").style.display="inline";
     document.getElementById("btn-regresar").style.display="none";
-    document.getElementById("info-schumacher").style.display="inline-block";
     document.getElementById("schumacher").src="michael.jpeg";
     document.getElementById("stats").style.display="none";
+    $("#sch2").css("display", "inline");
+    $("#container-preStats").css("display", "inline");
 }
